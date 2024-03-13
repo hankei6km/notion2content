cec13e439f74278efe48e9848103f537e26f8f5d
hankei6km <gh-hankei6km@outlook.jp>
2024-03-13 11:42:29 +0000

    fix: Fix handling of hast and mdast types

--- src/format.ts
+++ src/format.ts
@@ -1,4 +1,5 @@
 import matter from 'gray-matter'
+import type { Nodes } from 'hast-util-to-mdast/lib'
 import { toHtml as hastToHtml } from 'hast-util-to-html'
 import { toMdast as hastToMdast } from 'hast-util-to-mdast'
 import { toMarkdown as mdastToMarkdown } from 'mdast-util-to-markdown'
@@ -19,15 +20,26 @@ export async function toFrontmatterString(src: ContentRaw): Promise<string> {
   return '---\n---\n'
 }
 
+function isNodes(content: ContentRaw['content']): content is Nodes {
+  return (
+    content !== undefined &&
+    ((content as any).type === 'root' ||
+      (content as any).type === 'element' ||
+      (content as any).type === 'text' ||
+      (content as any).type === 'comment' ||
+      (content as any).type === 'doctype')
+  )
+}
+
 export async function toHtmlString(src: ContentRaw): Promise<string> {
-  if (src.content) {
+  if (isNodes(src.content)) {
     return hastToHtml(src.content)
   }
   return ''
 }
 
 export async function toHMarkdownString(src: ContentRaw): Promise<string> {
-  if (src.content) {
+  if (isNodes(src.content)) {
     return mdastToMarkdown(hastToMdast(src.content), {
       extensions: [gfmToMarkdown()]
     })
