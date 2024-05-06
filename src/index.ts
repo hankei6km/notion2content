16f954bbbcdf73c30fe639d60c153a95648efdb7
hankei6km <gh-hankei6km@outlook.jp>
2024-05-06 11:50:33 +0000

    feat: Export "Format" namespace

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { Client } from './lib/client.js'
 export { toContent } from './lib/notion2content.js'
+export { Format } from './format.js'
 export type { ToContentOpts } from './lib/types.js'
